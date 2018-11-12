c93895357d8b38bd560f42acfaa6f446e70b8325
jonnytaddesky <johnsmith20532@gmail.com>
2018-11-12 22:25:25 +0200

    zip genom

---
+++ app26.py
@@ -0,0 +1,14 @@
+s = input()
+
+cnt = 1
+
+for i in range(len(s)):
+    if i == len(s) - 1:
+        print(s[i] + str(cnt), end='')
+        break
+    elif s[i] == s[i + 1]:
+        cnt += 1
+    else:
+        print(s[i] + str(cnt), end='')
+        cnt = 1
+print()
